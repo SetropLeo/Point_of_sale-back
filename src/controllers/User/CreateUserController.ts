bedc6a7a2995ba76b70a104fc564e0f81b23e98f
LeonardoPortes <leol_portes@hotmail.com>
2022-05-30 19:33:50 -0300

    User controllers

---
+++ src/controllers/User/CreateUserController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import { CreateUserService } from "../../services/User/CreateUserService";
+
+export class CreateUserController {
+  async handle(request: Request, response: Response) {
+    const { first_name, last_name, email, password } = request.body;
+    const service = new CreateUserService();
+    const result = await service.execute({ first_name, last_name, email, password })
+
+    if (result instanceof Error) {
+      return response.status(400).json(result.message)
+    }
+
+    return response.json(result);
+  }
+}
