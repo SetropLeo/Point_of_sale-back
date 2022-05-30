600990588cc9914f5a5e0acdb1bef73ddcbbbf3c
LeonardoPortes <leol_portes@hotmail.com>
2022-05-30 19:30:55 -0300

    create base entitiy

---
+++ src/entities/Base.ts
@@ -0,0 +1,15 @@
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export abstract class BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP'})
+  updated_at: Date;
+
+  @Column({ type: 'timestamp', nullable: true })
+  deleted_at: Date;
+}
