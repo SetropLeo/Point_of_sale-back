12d34741db8da26af36b688f4dbec184d37fb51e
LeonardoPortes <leol_portes@hotmail.com>
2022-06-03 15:41:13 -0300

    Fix User service and controller

--- src/controllers/User/CreateUserController.ts
+++ src/controllers/User/CreateUserController.ts
@@ -3,9 +3,9 @@ import { CreateUserService } from "../../services/User/CreateUserService";
 
 export class CreateUserController {
   async handle(request: Request, response: Response) {
-    const { first_name, last_name, email, password } = request.body;
+    const { first_name, last_name, email } = request.body;
     const service = new CreateUserService();
-    const result = await service.execute({ first_name, last_name, email, password })
+    const result = await service.execute({ first_name, last_name, email })
 
     if (result instanceof Error) {
       return response.status(400).json(result.message)
--- src/services/User/CreateUserService.ts
+++ src/services/User/CreateUserService.ts
@@ -5,18 +5,16 @@ type UserRequest = {
   first_name: string;
   last_name: string;
   email: string;
-  password: string;
 }
 
 export class CreateUserService {
-  async execute({ first_name, last_name, email, password }: UserRequest): Promise<User | Error> {
+  async execute({ first_name, last_name, email }: UserRequest): Promise<User | Error> {
     const userRepo = getRepository(User);
 
     const user = userRepo.create({
       first_name,
       last_name,
       email,
-      password
     })
 
     await userRepo.save(user);
