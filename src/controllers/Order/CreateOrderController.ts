dd87b7af63be83b27f4f2a40983ca500c83077fd
LeonardoPortes <leol_portes@hotmail.com>
2022-06-11 15:48:59 -0300

    Fix Controller for order creation and Service for Get Orders

--- src/controllers/Order/CreateOrderController.ts
+++ src/controllers/Order/CreateOrderController.ts
@@ -6,7 +6,7 @@ export class CreateOrderController {
   async handle(request: Request, response: Response) {
     const { list, price, user_id } = request.body;
     const service = new CreateOrderService();
-    const result = await service.execute({ list, price, user_id });
+    const result = await service.execute({ price, user_id });
 
     if (result instanceof Error) {
       return response.status(400).json(result.message);
--- src/services/Order/GetAllOrderService.ts
+++ src/services/Order/GetAllOrderService.ts
@@ -4,11 +4,13 @@ import { Order } from "../../entities/Order";
 
 export class GetAllOrderService {
   async execute() {
-    const orderRepo = getRepository(Order);
 
-    const orders = await orderRepo.find({
-      relations: ['user'],
-    });
+    const orders = await 
+      getRepository(Order)
+      .createQueryBuilder("orders")
+      .take(10)
+      .orderBy("orders.created_at", "DESC")
+      .getMany();
 
     return orders;
   }
