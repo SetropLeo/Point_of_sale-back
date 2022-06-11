18e616c4a8cb43e6e25ce0726f01a537187e904d
LeonardoPortes <leol_portes@hotmail.com>
2022-06-11 15:47:19 -0300

    Add cors configuration

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,14 @@ import './database';
 import { routes } from './routes';
 
 const app = express();
+const cors = require('cors');
 app.use(express.json());
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(routes);
 
-app.listen(3000, () => console.log('Server running'));
+app.listen(3001, () => console.log('Server running'));
