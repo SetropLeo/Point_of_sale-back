a243a6ce6119b7891f02744da03dc0d186b0c18d
LeonardoPortes <leol_portes@hotmail.com>
2022-06-26 19:12:35 -0300

    Fix routes to use new controllers and new services

--- src/routes.ts
+++ src/routes.ts
@@ -1,40 +1,44 @@
 import { Router } from 'express';
-import CategoryController from './controllers/CategoryController';
-import FoodController from './controllers/FoodController';
-import UserController from './controllers/UserController';
-import { CreateDetailController } from './controllers/Detail/CreateDetailController';
-import { GetAllDetailController } from './controllers/Detail/GetAllDetailController';
-import { GetDetailController } from './controllers/Detail/GetDetailController';
-import { CreateOrderController } from './controllers/Order/CreateOrderController';
-import { GetAllOrderController } from './controllers/Order/GetAllOrderController';
+
 import CategoryService from './services/Category.service';
 import FoodService from './services/Food.service';
 import UserService from './services/User.Service';
+import OrderService from './services/Order.service';
+import OrderItemService from './services/Order-item.service';
+
+import OrderController from './controllers/OrderController';
+import OrderItemController from './controllers/OrderItemController';
+import CategoryController from './controllers/CategoryController';
+import FoodController from './controllers/FoodController';
+import UserController from './controllers/UserController';
 
 const foodController = new FoodController(new FoodService);
 const categoryController = new CategoryController(new CategoryService)
 const userController = new UserController(new UserService);
+const orderController = new OrderController(new OrderService);
+const orderItemController = new OrderItemController(new OrderItemService);
 
 const routes = Router();
 
-routes.post('/category/create', categoryController.create);
-routes.get('/category/getAll', categoryController.get);
-routes.put('/category/update/:id', categoryController.update);
-routes.delete('/category/delete/:id', categoryController.delete);
+routes.post('/category/', categoryController.create);
+routes.get('/category/', categoryController.get);
+routes.put('/category/:id', categoryController.update);
+routes.delete('/category/:id', categoryController.delete);
 
 routes.post('/food/', foodController.create);
 routes.get('/food/', foodController.get);
 routes.put('/food/:id', foodController.update);
 routes.delete('/food/:id', foodController.delete);
 
-routes.post('/order/create', new CreateOrderController().handle);
-routes.get('/order/getAll', new GetAllOrderController().handle);
+routes.post('/order/', orderController.create);
+routes.get('/order/', orderController.get);
+routes.get('/order/last10', orderController.getLast10);
 
-routes.post('/detail/create', new CreateDetailController().handle);
-routes.get('/detail/getAll', new GetAllDetailController().handle)
-routes.get('/detail/get/:order_id', new GetDetailController().handle)
+routes.post('/orderItem/', orderItemController.create);
+routes.get('/orderItem/', orderItemController.get);
+routes.get('/orderItem/:order_id', orderItemController.getById);
 
-routes.post('/user/create', userController.create);
-routes.get('/user/GetAll', userController.get);
+routes.post('/user/', userController.create);
+routes.get('/user/', userController.get);
 
 export { routes };
